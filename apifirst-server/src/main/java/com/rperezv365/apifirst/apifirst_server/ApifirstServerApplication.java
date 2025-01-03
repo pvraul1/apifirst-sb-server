24943f33ef4650d76a0a54160f99acebc28f0b29
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-01-03 11:41:14 +0100

    48. Add Spring Boot Module

---
+++ apifirst-server/src/main/java/com/rperezv365/apifirst/apifirst_server/ApifirstServerApplication.java
@@ -0,0 +1,13 @@
+package com.rperezv365.apifirst.apifirst_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifirstServerApplication.class, args);
+	}
+
+}
---
+++ apifirst-server/src/test/java/com/rperezv365/apifirst/apifirst_server/ApifirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rperezv365.apifirst.apifirst_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
